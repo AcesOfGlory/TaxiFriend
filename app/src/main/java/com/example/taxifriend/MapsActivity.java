c8153d900da701c58b6ff1aabe9850a5112e1426
CodeMaster8888 <mcmaster8888@gmail.com>
2019-03-01 09:36:17 +0000

    Map update

--- app/src/main/java/com/example/taxifriend/MapsActivity.java
+++ app/src/main/java/com/example/taxifriend/MapsActivity.java
@@ -1,7 +1,10 @@
 package com.example.taxifriend;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -22,6 +25,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        Button btn = (Button)findViewById(R.id.buttonMap);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MapsActivity.this, PaymentActivity.class));
+            }
+        });
     }
 
 
